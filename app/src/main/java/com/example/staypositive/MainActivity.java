4417a315a55cdb9af0ef092438c938ed30639dd2
evydsg <118639499+evydsg@users.noreply.github.com>
2024-02-09 18:43:41 -0800

    Second Activity Completed

--- app/src/main/java/com/example/staypositive/MainActivity.java
+++ app/src/main/java/com/example/staypositive/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.staypositive;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button clickAnywhereButton = findViewById(R.id.clickAnywhereButton);
+        clickAnywhereButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle click here
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+
+                // Start the new activity
+                startActivity(intent);
+            }
+        });
     }
 }
