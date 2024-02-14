4c820e90105cb50f07c740fddeb8ccb6ab194ee1
evydsg <118639499+evydsg@users.noreply.github.com>
2024-02-13 20:50:47 -0800

    Created buttons for RegisterActivity and LoginActivity

--- app/src/main/java/com/example/staypositive/welcomeActivity.java
+++ app/src/main/java/com/example/staypositive/welcomeActivity.java
@@ -2,13 +2,42 @@ package com.example.staypositive;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class welcomeActivity extends AppCompatActivity {
 
+    Button registerButton, loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        registerButton = findViewById(R.id.registerButton);
+            registerButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(welcomeActivity.this, RegisterActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+        loginButton = findViewById(R.id.loginButton);
+
+           loginButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Handle click here
+                    Intent intent = new Intent(welcomeActivity.this, LoginActivity.class);
+
+                    // Start the new activity
+                    startActivity(intent);
+                }
+            });
+        }
+
+
+
     }
-}
