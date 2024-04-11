45fbf0b5c5c83a064f35d9d6b13277093eb3e473
evydsg <118639499+evydsg@users.noreply.github.com>
2024-04-10 19:47:48 -0700

    Continued working on registerActivity: - Worked on Picker for DOB

--- app/src/main/java/com/example/staypositive/RegisterActivity.java
+++ app/src/main/java/com/example/staypositive/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.example.staypositive;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -10,6 +11,7 @@ import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
@@ -30,6 +32,9 @@ import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.time.Month;
+import java.util.Calendar;
+
 public class RegisterActivity extends AppCompatActivity {
     
     private EditText editTextRegisterName, editTextRegisterEmail, editTextRegisterDOB, editTextregisterPassword,
@@ -39,6 +44,8 @@ public class RegisterActivity extends AppCompatActivity {
     private RadioGroup radioGroupRegisterGender;
     private RadioButton radioButtonRegisterGenderSelected;
     private static final String TAG="RegsisterActivity";
+
+    private DatePickerDialog picker;
     
     
     @Override
@@ -55,6 +62,27 @@ public class RegisterActivity extends AppCompatActivity {
         //RadioButton for Gender
         radioGroupRegisterGender = findViewById(R.id.radio_group_register_gender);
         radioGroupRegisterGender.clearCheck();
+
+        editTextRegisterDOB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar calendar = Calendar.getInstance();
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(Calendar.YEAR);
+
+                //Date PickerDialog
+
+                picker = new DatePickerDialog(RegisterActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        editTextRegisterDOB.setText((month + 1) + "/" + dayOfMonth + '/' + year);
+
+                    }
+                }, year, month, day);
+                picker.show();
+            }
+        });
 
         progressBar = findViewById(R.id.progressBar);
         Button buttonRegister = findViewById(R.id.signUp);
