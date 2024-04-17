4a0f4cc3ec409bedaff92bde09e618da4ac02714
evydsg <118639499+evydsg@users.noreply.github.com>
2024-04-17 13:21:46 -0700

    Worked on Login Screen: - Added Hide/Show Password Feature - Performed use case tests

--- app/src/main/java/com/example/staypositive/LoginActivity.java
+++ app/src/main/java/com/example/staypositive/LoginActivity.java
@@ -1,14 +1,130 @@
 package com.example.staypositive;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+
+import java.util.logging.Handler;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextLoginEmail, editTextLoginPwd;
+    private ProgressBar progressBar;
+    private FirebaseAuth authProfile;
+    private static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editTextLoginEmail = findViewById(R.id.editText_registered_email);
+        editTextLoginPwd = findViewById(R.id.editText_registered_password);
+        progressBar = findViewById(R.id.progressBar);
+
+        authProfile = FirebaseAuth.getInstance();
+
+        Button buttonLogin = findViewById(R.id.signIn);
+
+        //
+        ImageView imageViewShowHidePwd = findViewById(R.id.imageView_show_hide_pwd);
+        imageViewShowHidePwd.setImageResource(R.drawable.ic_show_pwd);
+        imageViewShowHidePwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(editTextLoginPwd.getTransformationMethod().equals(HideReturnsTransformationMethod.getInstance()))
+                {
+                    //If password is visible then Hide it
+                    editTextLoginPwd.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    //Change icon
+                    imageViewShowHidePwd.setImageResource(R.drawable.ic_show_pwd);
+                }else {
+                    editTextLoginPwd.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    imageViewShowHidePwd.setImageResource(R.drawable.ic_hide_pwd);
+                }
+            }
+        });
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textEmail = editTextLoginEmail.getText().toString();
+                String textPwd = editTextLoginPwd.getText().toString();
+
+                if(TextUtils.isEmpty(textEmail))
+                {
+                    Toast.makeText(LoginActivity.this, "Please enter your email.", Toast.LENGTH_SHORT).show();
+                    editTextLoginEmail.setError("Email is required.");
+                    editTextLoginEmail.requestFocus();
+                }else if(!Patterns.EMAIL_ADDRESS.matcher(textEmail).matches())
+                {
+                    Toast.makeText(LoginActivity.this,"Please enter a valid email address.", Toast.LENGTH_SHORT).show();
+                    editTextLoginEmail.setError("Valid email address is required.");
+                    editTextLoginEmail.requestFocus();
+                }else if(TextUtils.isEmpty(textPwd))
+                {
+                    Toast.makeText(LoginActivity.this, "Please enter your password.",Toast.LENGTH_SHORT).show();
+                    editTextLoginPwd.setError("Password is required.");
+                    editTextLoginPwd.requestFocus();
+                }else {
+                    progressBar.setVisibility(View.VISIBLE);
+                    loginUser(textEmail, textPwd);
+                }
+            }
+        });
+    }
+
+    private void loginUser(String email, String pwd)
+    {
+        authProfile.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(LoginActivity.this,new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful())
+                {
+                    Toast.makeText(LoginActivity.this, "You are signed in.", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, MainProfile.class);
+                    // Start the new activity
+                    startActivity(intent);
+                } else {
+                    try{
+                        throw task.getException();
+                    }catch (FirebaseAuthInvalidUserException e) {
+                        editTextLoginEmail.setError("User does not exists or is no longer valid. Please register again.");
+                        editTextLoginEmail.requestFocus();
+                    }catch (FirebaseAuthInvalidCredentialsException e) {
+                        editTextLoginEmail.setError("Invalid credentials. Kindly, check and re-enter.");
+                        editTextLoginEmail.requestFocus();
+                    }catch (Exception e)
+                    {
+                        Log.e(TAG, e.getMessage());
+                        Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+
     }
 }
