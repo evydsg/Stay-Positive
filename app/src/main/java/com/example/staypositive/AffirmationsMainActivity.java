73ecf5fdf71d18d27b95b9135858ac3ebae2485c
evydsg <evelisekguenda@gmail.com>
2024-06-12 09:18:25 -0700

    Things completed: - Fixed the issue with GeneralAffirmationsActivity - Added affirmations in the activity

--- app/src/main/java/com/example/staypositive/AffirmationsMainActivity.java
+++ app/src/main/java/com/example/staypositive/AffirmationsMainActivity.java
@@ -19,6 +19,7 @@ public class AffirmationsMainActivity extends AppCompatActivity {
 
     FirebaseAuth authProfile;
     Button general_Button, favoritesButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,73 +36,64 @@ public class AffirmationsMainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
         favoritesButton = findViewById(R.id.favoritesButton);
         favoritesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(AffirmationsMainActivity.this, GeneralAffirmationsActivity.class);
+                // Assuming this should navigate to a different activity
+                Intent intent = new Intent(AffirmationsMainActivity.this, favoritesAffirmations.class);
                 startActivity(intent);
             }
         });
     }
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        //Inflate Menu Items
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate Menu Items
         getMenuInflater().inflate(R.menu.common_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
-    //When any option is selected
+    // When any option is selected
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
 
-        if (id == android.R.id.home)
-        {
+        if (id == android.R.id.home) {
             NavUtils.navigateUpFromSameTask(AffirmationsMainActivity.this);
-        }
-        else if(id == R.id.refresh_menu)
-        {
-            //Refresh Activity
+        } else if (id == R.id.refresh_menu) {
+            // Refresh Activity
             startActivity(getIntent());
             finish();
-            overridePendingTransition(0,0);
-        }else if(id ==R.id.update_profile)
-        {
+            overridePendingTransition(0, 0);
+        } else if (id == R.id.update_profile) {
             Intent intent = new Intent(AffirmationsMainActivity.this, UpdateProfileActivity.class);
             startActivity(intent);
             finish();
-        }else if(id ==R.id.update_email)
-        {
+        } else if (id == R.id.update_email) {
             Intent intent = new Intent(AffirmationsMainActivity.this, UpdateEmailActivity.class);
             startActivity(intent);
-        }else if(id == R.id.settings)
-        {
+        } else if (id == R.id.settings) {
             Toast.makeText(AffirmationsMainActivity.this, "Menu Settings", Toast.LENGTH_LONG).show();
-        }else if(id == R.id.change_password)
-        {
+        } else if (id == R.id.change_password) {
             Intent intent = new Intent(AffirmationsMainActivity.this, ChangePasswordActivity.class);
             startActivity(intent);
             finish();
-        }
-        else if(id == R.id.delete_account)
-        {
+        } else if (id == R.id.delete_account) {
             Intent intent = new Intent(AffirmationsMainActivity.this, DeleteAccount.class);
             startActivity(intent);
             finish();
-        }
-        else if(id == R.id.logout)
-        {
+        } else if (id == R.id.logout) {
             authProfile.signOut();
             Toast.makeText(AffirmationsMainActivity.this, "Sign Out", Toast.LENGTH_LONG).show();
             Intent intent = new Intent(AffirmationsMainActivity.this, MainActivity.class);
 
-            //Clear stack to prevent user from coming back to MainProfile Activity
+            // Clear stack to prevent user from coming back to MainProfile Activity
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
             finish();
-        }else {
+        } else {
             Toast.makeText(AffirmationsMainActivity.this, "Something went wrong!", Toast.LENGTH_LONG).show();
         }
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/example/staypositive/GeneralAffirmationsActivity.java
+++ app/src/main/java/com/example/staypositive/GeneralAffirmationsActivity.java
@@ -3,103 +3,75 @@ package com.example.staypositive;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NavUtils;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewTreeObserver;
-import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.ScrollView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class GeneralAffirmationsActivity extends AppCompatActivity {
 
     FirebaseAuth authProfile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_general_affirmations); // Ensure the correct layout file is used
 
-
+        authProfile = FirebaseAuth.getInstance(); // Initialize FirebaseAuth
+        Log.d("GeneralAffirmationsActivity", "onCreate called");
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        //Inflate Menu Items
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate Menu Items
         getMenuInflater().inflate(R.menu.common_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
-    //When any option is selected
+    // When any option is selected
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
 
-        if(id== android.R.id.home)
-        {
+        if (id == android.R.id.home) {
             NavUtils.navigateUpFromSameTask(GeneralAffirmationsActivity.this);
-        }
-        else if(id == R.id.refresh_menu)
-        {
-            //Refresh Activity
+        } else if (id == R.id.refresh_menu) {
+            // Refresh Activity
             startActivity(getIntent());
             finish();
-            overridePendingTransition(0,0);
-        }else if(id ==R.id.update_profile)
-        {
+            overridePendingTransition(0, 0);
+        } else if (id == R.id.update_profile) {
             Intent intent = new Intent(GeneralAffirmationsActivity.this, UpdateProfileActivity.class);
             startActivity(intent);
             finish();
-        }else if(id ==R.id.update_email)
-        {
+        } else if (id == R.id.update_email) {
             Intent intent = new Intent(GeneralAffirmationsActivity.this, UpdateEmailActivity.class);
             startActivity(intent);
-        }else if(id == R.id.settings)
-        {
+        } else if (id == R.id.settings) {
             Toast.makeText(GeneralAffirmationsActivity.this, "Menu Settings", Toast.LENGTH_LONG).show();
-        }else if(id == R.id.change_password)
-        {
+        } else if (id == R.id.change_password) {
             Intent intent = new Intent(GeneralAffirmationsActivity.this, ChangePasswordActivity.class);
             startActivity(intent);
             finish();
-        }
-        else if(id == R.id.delete_account)
-        {
+        } else if (id == R.id.delete_account) {
             Intent intent = new Intent(GeneralAffirmationsActivity.this, DeleteAccount.class);
             startActivity(intent);
             finish();
-        }
-        else if(id == R.id.logout)
-        {
+        } else if (id == R.id.logout) {
             authProfile.signOut();
             Toast.makeText(GeneralAffirmationsActivity.this, "Sign Out", Toast.LENGTH_LONG).show();
             Intent intent = new Intent(GeneralAffirmationsActivity.this, MainActivity.class);
 
-            //Clear stack to prevent user from coming back to MainProfile Activity
+            // Clear stack to prevent user from coming back to MainProfile Activity
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
             finish();
-        }else {
+        } else {
             Toast.makeText(GeneralAffirmationsActivity.this, "Something went wrong!", Toast.LENGTH_LONG).show();
         }
         return super.onOptionsItemSelected(item);
